6f3b541bbddfae115aa283550ce62b82b19eae4b
Jiraporn Jaroensutiyotin <jjaroens@z2t2c2.42bangkok.com>
2024-02-25 17:23:22 +0700

    add test3.c

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jjaroens <jjaroens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 20:13:37 by jjaroens          #+#    #+#             */
-/*   Updated: 2024/02/18 17:58:14 by jjaroens         ###   ########.fr       */
+/*   Updated: 2024/02/25 15:45:41 by jjaroens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,13 @@
 #include <stdlib.h>
 //#include "libft/libft.h"
 
-typedef struct s_stack
+typedef struct s_list
 {
 	int				value;
-	struct s_stack	*next; //storing the address of the previous node
-	struct s_stack	*previous; //storing the address of the last node
-}	t_stack;
+	struct s_list	*next;
+	struct s_list	*previous;
+} t_node;
+
 
 // stack && heap
 #endif
